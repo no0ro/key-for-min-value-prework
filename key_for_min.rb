5424b095e8052702496843394978c7acb6840f7b
Noelle Rolwes <nrolwes@gmail.com>
2018-10-05 19:16:43 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,13 @@
 
 def key_for_min_value(name_hash)
   if name_hash = {}
-    return nil 
+    nil 
   else 
     small_key = nil
     small_value = nil
     
     name_hash.each do |key, value|
-      if value <= small_value
+      if value < small_value
         small_value = value 
         small_key = key 
       end
