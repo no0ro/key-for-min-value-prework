ee7ad0ee9939439cfd4e646fc918fda0e7a9dda1
Noelle Rolwes <nrolwes@gmail.com>
2018-10-05 19:23:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
     small_value = nil
   
     name_hash.each do |key, value|
-      if value < small_value
+      if value <= small_value
         small_value = value 
         small_key = key 
       end
